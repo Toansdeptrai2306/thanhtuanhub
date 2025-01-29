9ca016fe5133eac0741ad974b0e8524c55628396
Toansdeptrai2306 <nguyenminhtoan2306t@gmail.com>
2025-01-29 11:54:45 +0700

    Create TtHub.lua

---
+++ TtHub.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Thanh Tuấn Hub",--Tên Script
+    logoscript = "73554279980224",--ID Logo Script
+    tacgia = "Thanhh Tuấn "-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Toansdeptrai2306/thanhtuanhub/refs/heads/main/Thanhtuan.lua"))()
